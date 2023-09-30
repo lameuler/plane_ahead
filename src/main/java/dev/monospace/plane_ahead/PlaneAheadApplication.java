620041a2d2b4690e3213fd6a375592735aa96ff9
GreeneryScenery <89194387+GreeneryScenery@users.noreply.github.com>
2023-09-30 16:18:07 +0800

    Deleted hello-view and renamed HelloApplication to PlaneAheadApplication

--- src/main/java/dev/monospace/plane_ahead/PlaneAheadApplication.java
+++ src/main/java/dev/monospace/plane_ahead/PlaneAheadApplication.java
@@ -3,18 +3,14 @@ package dev.monospace.plane_ahead;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class PlaneAheadApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("airport-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(PlaneAheadApplication.class.getResource("airport-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
         stage.setTitle("Plane Ahead!");
