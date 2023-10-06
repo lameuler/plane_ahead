c70e56c93c2ec97f691dccbb80bc00687f951104
GreeneryScenery <89194387+GreeneryScenery@users.noreply.github.com>
2023-10-06 14:29:48 +0800

    Minor adjustments to stage
    Set scene size to screen size and removed exit full screen.

--- src/main/java/dev/monospace/plane_ahead/PlaneAheadApplication.java
+++ src/main/java/dev/monospace/plane_ahead/PlaneAheadApplication.java
@@ -5,23 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.awt.*;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class PlaneAheadApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PlaneAheadApplication.class.getResource("airport-view.fxml"));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 1280, 720);
-        scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
+        Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Scene scene = new Scene(root, ScreenSize.getWidth(), ScreenSize.getHeight());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("app.css")).toExternalForm());
         stage.setTitle("Plane Ahead!");
         stage.setScene(scene);
-        stage.show();
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setFullScreen(true);
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         stage.setResizable(false);
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon.png"))));
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module dev.monospace.plane_ahead {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
 
     opens dev.monospace.plane_ahead to javafx.fxml;
     exports dev.monospace.plane_ahead;
